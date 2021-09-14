5b327f6394f4938921008bd8731458899f5efbca
Awtry <jorge_verah@outlook.es>
2021-09-14 18:11:31 -0500

    Creación de fragmentos
    Comienzo de despliegue

---
+++ app/src/main/java/com/awtry/ex_primer_parcial/Frag_Detalle.kt
@@ -0,0 +1,11 @@
+package com.awtry.ex_primer_parcial
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Frag_Detalle : Fragment() {
+
+}
--- app/src/main/java/com/awtry/ex_primer_parcial/MainActivity.kt
+++ app/src/main/java/com/awtry/ex_primer_parcial/MainActivity.kt
@@ -4,8 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private val KEY = "STATE_KEY"
+    private var Go_Login: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Go_Login = savedInstanceState?.getBoolean(KEY,true) ?: true
+        if (Go_Login)
+            supportFragmentManager.beginTransaction().add(R.id.Padre, Frag_login()).commit()
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run{
+            putBoolean(KEY, false)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+
+
 }
